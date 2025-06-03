98d1f551ba6dbd1eedd3a360128116d5ea93448b
Juan Manuel Lantermo <juanmlantermo@gmail.com>
2025-06-03 19:07:31 -0300

    users-endpoints and functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from './config.js';
 import { sequelize } from './db.js';
 import routineRoutes from './routes/routines.routes.js';
@@ -9,18 +10,22 @@ import "./model/User.js";
 import "./model/Exercise.js";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.json());
+app.use(routineRoutes);
+app.use(userRoutes);
+app.use(exerciseRoutes);
 
-try {
-  app.use(express.json());
-  app.listen(PORT);
-  app.use(routineRoutes);
-  app.use(userRoutes);
-  app.use(exerciseRoutes);
-
-  await sequelize.sync();
-  
-  console.log(`Server is running on port ${PORT}`);
-} catch (error) {
-  console.error('Error starting the server:', error);
-  
+async function main() {
+  try {
+    await sequelize.sync({ alter: true });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
 }
+
+main();
